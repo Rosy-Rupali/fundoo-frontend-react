db692cffa743a9eb7b69c6e1252d61da8f2c01c4
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-08-27 23:13:57 +0530

    initial commit

---
+++ fundoo-notes/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/Login';
+import SignIn from './pages/SignIn';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LoginPage /> */}
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
