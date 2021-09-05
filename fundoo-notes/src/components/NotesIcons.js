642e76458d805e5c4692215d3e5f5a46cffdaa97
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-09-06 04:54:36 +0530

    added: new component note to the app which conatins icons, title and textarea

---
+++ fundoo-notes/src/components/NotesIcons.js
@@ -0,0 +1,24 @@
+import React from "react";
+import AddAlertOutlinedIcon from "@material-ui/icons/AddAlertOutlined";
+import PersonAddOutlinedIcon from "@material-ui/icons/PersonAddOutlined";
+import ColorLensOutlinedIcon from "@material-ui/icons/ColorLensOutlined";
+import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
+import ArchiveOutlinedIcon from "@material-ui/icons/ArchiveOutlined";
+import MoreVertOutlinedIcon from "@material-ui/icons/MoreVertOutlined";
+import "../css/NotesIcons.css";
+
+const NotesIcons = () => {
+  return (
+    <div className="displayNotesIcons-mainContainer">
+      <div className="displayNotes-icons">
+        <AddAlertOutlinedIcon style={{ fontSize: "medium" }} />
+        <PersonAddOutlinedIcon style={{ fontSize: "medium" }} />
+        <ColorLensOutlinedIcon style={{ fontSize: "medium" }} />
+        <ImageOutlinedIcon style={{ fontSize: "medium" }} />
+        <ArchiveOutlinedIcon style={{ fontSize: "medium" }} />
+        <MoreVertOutlinedIcon style={{ fontSize: "medium" }} />
+      </div>
+    </div>
+  );
+};
+export default NotesIcons;
