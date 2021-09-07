60ed9a2b6ceb843066703bff505d0b5022963d2a
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-09-08 02:46:53 +0530

    added: the snackbar to the archive icon

---
+++ fundoo-notes/src/components/ArchiveIcon.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ArchiveOutlinedIcon from "@material-ui/icons/ArchiveOutlined";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+
+export default function SimpleSnackbar(props) {
+  const [open, setOpen] = React.useState(false);
+  const [isArchive, setIsArchive] = React.useState(false)
+  const handleClick = () => {
+    setIsArchive(!isArchive)
+    props.archive(isArchive)
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <ArchiveOutlinedIcon
+        style={{ fontSize: "medium" }}
+        onClick={handleClick} 
+      />
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        open={open}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        message="Note archived"
+        action={
+          <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+              UNDO 
+            </Button>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+    </div>
+  );
+}
