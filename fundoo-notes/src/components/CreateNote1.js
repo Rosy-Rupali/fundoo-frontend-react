9199e14b5db8108abaa4b65153952ba65f276543
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-09-11 02:45:20 +0530

    added: the dashboard header, createnote1, createnote2, displaynotes and drawer component to the dashboard component

--- fundoo-notes/src/components/CreateNote1.js
+++ fundoo-notes/src/components/CreateNote1.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined";
 import ImageOutlinedIcon from "@material-ui/icons/ImageOutlined";
 import BrushOutlinedIcon from "@material-ui/icons/BrushOutlined";
 import "../css/CreateNote1.css";
 
-const CreateNote1 = () => {
+const CreateNote1 = (props) => {
+  const WritingNote = () => {
+    props.listentoNote1('true')
+  }
+  useEffect(() =>{
+    console.log(props)
+  },[props])
   return (
-    <div className="dummy-box">
+    <div className="dummy-box" onClick={WritingNote}>
       <div className="dummy-createnote1">
         <p id="para-createnote1">Take a note. . .</p>
         <div className="icons-createnote1">
