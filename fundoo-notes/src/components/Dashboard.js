a76fc177078577297c0eaf6e678fb7487b36fb2b
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-09-10 09:57:49 +0530

    refactored: added the code for side navbar using drawer

--- fundoo-notes/src/components/Dashboard.js
+++ fundoo-notes/src/components/Dashboard.js
@@ -3,44 +3,139 @@ import DashboardHeader from "./DashboardHeader";
 import CreateNote1 from "./CreateNote1";
 import CreateNote2 from "./CreateNote2";
 import CreateNote3 from "./CreateNote3";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import NotificationsNoneSharpIcon from "@material-ui/icons/NotificationsNoneSharp";
+import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
+import ArchiveOutlinedIcon from "@material-ui/icons/ArchiveOutlined";
+import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import DisplayNotesNew from "./DisplayNotesNew";
 import "../css/DashboardMain.css";
+import "../css/SideNavbar.css";
 
-function DashBoard() {
-  const [openNote, setOpenNote] = useState("one");
+const useStyles = makeStyles((theme) => ({
+  hide: {
+    display: "block",
+  },
+  drawer: {
+    width: '15vw',
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: '15vw',
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+
+    border: "none !important",
+    paddingLeft: 0,
+    boxShadow: "0rem 0.5rem 1rem rgba(100,100,100,0.1) !important",
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: 6,
+      marginTop: 64,
+    },
+    marginTop: 55,
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(6.7),
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(8.5) + 1,
+      paddingLeft: 6,
+      marginTop: 64,
+    },
+    marginTop: 55,
+    border: "none !important",
+  },
+}));
+function DashBoard(props) {
+  const [openNote, setOpenNote] = useState(false);
+  const [handleDrawer1, setHandleDrawer] = useState(false);
+  const classes = useStyles();
+
+  const handleDrawer = () => {
+    setHandleDrawer(!handleDrawer1);
+  };
 
   const inputNote1 = (data) => {
     if (data === "true") {
-      setOpenNote("two");
+      setOpenNote(!openNote);
     }
   };
   const inputNote2 = (data) => {
     if (data === "true") {
-      setOpenNote("one");
-    } else if (data === "collab") {
-      setOpenNote("collab");
+      setOpenNote(!openNote);
     }
   };
-
+  const handleClickAwayEvent = () => {};
   return (
     <div class="dashboard-main-container">
       <div className="dashboard-header">
-        <DashboardHeader />
+        <DashboardHeader handleDrawer={handleDrawer} />
       </div>
+      <ClickAwayListener onClickAway={handleClickAwayEvent}>
+        <div className="container-sideNavBar">
+          <Drawer
+            open={handleDrawer1}
+            variant="persistent"
+            className={clsx(classes.drawer, {
+              [classes.drawerOpen]: props.handleDrawer1,
+              [classes.drawerClose]: !props.handleDrawer1,
+            })}
+            classes={{
+              paper: clsx({
+                [classes.drawerOpen]: props.handleDrawer1,
+                [classes.drawerClose]: !props.handleDrawer1,
+              }),
+            }}
+          >
+            {
+              <div className="mainConatiner-drawer">
+                <div className="testContainer-drawer">
+                  <div id="block1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="rgb(75, 72, 72)"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6A4.997 4.997 0 0 1 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z"></path>
+                    </svg>
+                    <NotificationsNoneSharpIcon />
+                    <CreateOutlinedIcon />
+                    <ArchiveOutlinedIcon />
+                    <DeleteOutlinedIcon />
+                  </div>
+                  <div id="block2">
+                    <span>Notes</span>
+                    <span>Reminders</span>
+                    <span>Edit Labels</span>
+                    <span>Archive</span>
+                    <span>Trash</span>
+                  </div>
+                </div>
+              </div>
+            }
+          </Drawer>
+        </div>
+      </ClickAwayListener>
       <div className="createnote-container">
-        {openNote == "two" ? (
+        {openNote ? (
           <CreateNote2 listentoNote2={inputNote2} />
-        ) : openNote == "one" ? (
-          <CreateNote1 listentoNote1={inputNote1} />
-        ) : openNote == "collab" ? (
-          <CreateNote3 />
         ) : (
-          <div> </div>
+          <CreateNote1 listentoNote1={inputNote1} />
         )}
       </div>
-      <div className="displaynote-container">
-        <DisplayNotesNew />
-      </div>
+      <div className="displaynote-container">{/* <DisplayNotesNew /> */}</div>
     </div>
   );
 }
--- fundoo-notes/src/components/DashboardHeader.jsx
+++ fundoo-notes/src/components/DashboardHeader.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
-import DehazeIcon from '@material-ui/icons/Dehaze';
+import React, { useState } from "react";
+import DehazeIcon from "@material-ui/icons/Dehaze";
 import SearchBar from "material-ui-search-bar";
-import RefreshRoundedIcon from '@material-ui/icons/RefreshRounded';
-import ViewAgendaOutlinedIcon from '@material-ui/icons/ViewAgendaOutlined';
-import SettingsIcon from '@material-ui/icons/Settings';
-import AppsIcon from '@material-ui/icons/Apps';
-import '../css/DashboardHeader.css'
+import RefreshRoundedIcon from "@material-ui/icons/RefreshRounded";
+import ViewAgendaOutlinedIcon from "@material-ui/icons/ViewAgendaOutlined";
+import SettingsIcon from "@material-ui/icons/Settings";
+import AppsIcon from "@material-ui/icons/Apps";
+import "../css/DashboardHeader.css";
+import { getThemeProps } from "@material-ui/styles";
 
-function DashboardHeader(){
-
-return(
-        <div className="header">
-            <div className="dashboard" id="text-icons">
-                <DehazeIcon></DehazeIcon>
-                <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"></img>
-                <span>Keep</span>
-                </div>
-                <div className="searchsection">
-                <SearchBar placeholder="search" />
-                </div>
-                <div className="dashboard">
-                <RefreshRoundedIcon></RefreshRoundedIcon>
-                <ViewAgendaOutlinedIcon className="icons"></ViewAgendaOutlinedIcon>
-                <SettingsIcon></SettingsIcon>
-                </div>
-                <div className="dashboard">
-                <AppsIcon></AppsIcon>
-                <img src="https://lh3.googleusercontent.com/ogw/ADea4I6JTDF1nhao1eahgtEuabHZOUDW5B54Zb2hSLx4=s32-c-mo" style={{width:'50px',borderRadius: '50%'}}></img>
-            </div>
+function DashboardHeader(props) {
+  const navigationtoggle = () => {
+    props.handleDrawer();
+  }
+  return (
+    <div className="header">
+      <div className="dashboard" id="text-icons">
+        <DehazeIcon onClick={navigationtoggle} />
+        <img
+          src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
+          alt="logo2"
+        ></img>
+        <span>Keep</span>
+      </div>
+      <div className="searchsection">
+        <SearchBar placeholder="search" />
+      </div>
+      <div className="dashboard">
+        <RefreshRoundedIcon />
+        <ViewAgendaOutlinedIcon className="icons" />
+        <SettingsIcon />
+      </div>
+      <div className="dashboard">
+        <AppsIcon />
+        <img
+          src="https://lh3.googleusercontent.com/ogw/ADea4I6JTDF1nhao1eahgtEuabHZOUDW5B54Zb2hSLx4=s32-c-mo"
+          style={{ width: "50px", borderRadius: "50%" }}
+          alt="logo3"
+        ></img>
+      </div>
     </div>
-    )
+  );
 }
 export default DashboardHeader;
